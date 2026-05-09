d16792e2156c8d60a33c807624cc4c21f7114dc7
tarvel <taiyebabatundejames@gmail.com>
2026-05-10 00:30:06 +0100

    debug = true

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,8 @@ load_dotenv()
 # Security
 # ============================================================
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+# 'DEBUG', 
+DEBUG = config(default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
